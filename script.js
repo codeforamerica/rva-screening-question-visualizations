92e85f85fa71090cdd13aa1f6b50b9b02df67810
Benjamin Golder <benjamin.j.golder@gmail.com>
2015-05-06 18:24:49 -0700

    added a lot more, no glyphicons

--- script.js
+++ script.js
@@ -17,8 +17,8 @@ function postProcess(data){
   drawChart();
 }
    
-function getField(key){
-  return _.findWhere(FIELDS, {"key":key});
+function getField(slug){
+  return _.findWhere(FIELDS, {"slug":slug});
 }
 
 function processParents(){
@@ -32,18 +32,60 @@ function processParents(){
         if( _.has(parentField, "children") ){
           parentField.children.push(field);
         } else {
-          parentField.children = [field];
+          parentField.children = [];
+          parentField.children.push(field);
         }
       }
     }
   });
 }
 
+function getParents(field){
+  var parents = [];
+  var parent_to_check = true;
+  while (parent_to_check) {
+    if( field.parent && field.parent != "" ){
+      // add parent
+      parents.push(field.parent);
+      // set new field
+      field = field.parent;
+    } else {
+      parent_to_check = false
+    }
+  }
+  return parents.reverse();
+}
+
 function drawChart(){
-  ulElement = d3.select(".chart").append("ul");
-  fieldElements = ulElement.selectAll("li").data(FIELDS).enter()
+  var ulElement = d3.select(".chart").append("ul");
+  var fieldElements = ulElement.selectAll("li").data(FIELDS).enter()
     .append("li")
-    .text(function(d){return d.question;});
+    .attr("class", function(d){
+      return "status-" + d["status"];
+    });
+
+  fieldElements.append("span")
+    .attr("class", "question")
+    .text(function(d){return "“"+d.question+"”";});
+
+  fieldElements.append("span")
+    .attr("class", "slug")
+    .text(function(d){return d.slug;});
+
+  fieldElements.selectAll(".parent")
+    .data(function(d){ return getParents(d);}).enter()
+    .insert("span", ":first-child")
+    .attr("class", "parent")
+    .text("◤");
+
+  fieldElements.append("span")
+    .attr("class", "children")
+    .text(function(d){
+      if( d.children && d.children.length > 0 ){
+        return "◢";
+      }
+    })
+
 }
 
 
