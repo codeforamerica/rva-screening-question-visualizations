1544187f671c493a7c0aec8952943e4d0a95b7de
Benjamin Golder <benjamin.j.golder@gmail.com>
2015-05-11 16:26:39 -0700

    updated to show question display dependencies

--- script.js
+++ script.js
@@ -56,13 +56,54 @@ function getParents(field){
   return parents.reverse();
 }
 
+function selectParentDiv(d){
+  return d3.select("#"+d.parents[0].slug);
+}
+
 function drawChart(){
-  var ulElement = d3.select(".chart").append("ul");
-  var fieldElements = ulElement.selectAll("li").data(FIELDS).enter()
-    .append("li")
+
+  var parentFields = FIELDS.filter(function(d){ return d.children; });
+
+  var tree = d3.layout.tree().nodeSize([0,3]);
+  var root = {
+    "id": 0,
+    "question": "All Questions with Dependencies",
+    "slug": "all_dependencies",
+    "children": parentFields
+  };
+  var nodes = tree.nodes(root);
+  var entryDiv = d3.select(".chart");
+  var nodeDivs = entryDiv.selectAll("div.node")
+    .data(nodes, function(d, i) { return d.id; })
+    .enter().append("div").attr("class", function(d){
+      return "node status-"+d.status;
+    })
+    .attr("id", function(d){ return d.slug; })
+    .style("padding-left", function(d){
+      return (getParents(d).length * 2) + "rem";
+    });
+  nodeDivs.append("span").attr("class", "slug").text(function(d){ 
+      return d.slug; 
+    });
+  nodeDivs.append("span").attr("class", "question").text(function(d){
+    return '“'+d.question+'”';
+  });
+  nodeDivs.on("mouseenter", function(d,i){
+    d3.select("#"+d.parent.slug)
+      .append("span").attr("class", "condition-note")
+      .text(d.condition);
+  }).on("mouseleave", function(d){
+    d3.selectAll( "#"+d.parent.slug+" .condition-note")
+      .remove();
+  });
+
+/*
+  var fieldElements = ulElement.selectAll("li").data(
+    ).append("li")
     .attr("class", function(d){
       return "status-" + d["status"];
     });
+
 
   fieldElements.append("span")
     .attr("class", "question")
@@ -85,6 +126,7 @@ function drawChart(){
         return "◢";
       }
     })
+    */
 
 }
 
